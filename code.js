c2038199e78e1891c56dcc36fc38ca45f348176b
royapouya <royapouya2656@gmail.com>
2025-06-05 11:39:37 +0330

    Update code.js - 4

--- code.js
+++ code.js
@@ -1,14 +1,32 @@
-//selector
+const todoNameInput = document.querySelector('.todo-name-input')
+const todoMobileInput = document.querySelector('.todo-mobile-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
+const selectBox = document.querySelector('.type-todo')
 
-const todoNameInput = document.querySelector('.todo-name-input');
-const todoMobileInput = document.querySelector('.todo-mobile-input');
-const todoButton = document.querySelector('.todo-button');
-
-//event listener
-todoButton.addEventListener('click', addItem);
+//event listner
+todoButton.addEventListener('click', addItem)
 
 //function
-function addItem(event) {
-    event.preventDefault();
-    console.log(event);
+function addItem(event){
+    event.preventDefault()
+    console.log(event)
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo')
+
+    //create li
+    const newItem = document.createElement('li');
+    const nameSpan = document.createElement('span')
+    const mobileSpan = document.createElement('span')
+    const typeSpan = document.createElement('span')
+    nameSpan.innerText = todoNameInput.value;
+    mobileSpan.innerText = todoMobileInput.value;
+    typeSpan.innerText = selectBox.options[selectBox.selectedIndex].innerText
+    newItem.appendChild(nameSpan)
+    newItem.appendChild(mobileSpan)
+    newItem.appendChild(typeSpan)
+    newItem.classList.add('todo-item')
+    todoDiv.appendChild(newItem)
+    //append div to ul
+    todoList.appendChild(todoDiv)
 }
