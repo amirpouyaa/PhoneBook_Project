9e38e8b56d5721486db36bb8f5ec6f2953c07ac6
royapouya <royapouya2656@gmail.com>
2025-06-05 12:09:26 +0330

    Update code.js - 5

--- code.js
+++ code.js
@@ -6,6 +6,7 @@ const selectBox = document.querySelector('.type-todo')
 
 //event listner
 todoButton.addEventListener('click', addItem)
+todoList.addEventListener('click', deleteItem);
 
 //function
 function addItem(event){
@@ -27,6 +28,25 @@ function addItem(event){
     newItem.appendChild(typeSpan)
     newItem.classList.add('todo-item')
     todoDiv.appendChild(newItem)
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-button');
+    todoDiv.appendChild(trashButton);
     //append div to ul
     todoList.appendChild(todoDiv)
+
+    todoMobileInput.value = '';
+    todoNameInput.value = '';
 }
+function deleteItem(event) {
+    console.log(event.target);
+    const item = event.target;
+    if (item.classList[0] === 'trash-button') {
+      const contact = item.parentElement;
+      contact.classList.add('fall');
+      setTimeout(() => {
+        contact.remove();
+      },2000);
+    }
+  }
